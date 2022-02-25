95194b7ec42278f5e11a8026ca121d31d8bd1092
Jaeheon-So <sjh-98@naver.com>
2022-02-25 17:58:41 +0900

    Add header, button layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//pages
 import Home from "./pages/Home";
 import Edit from "./pages/Edit";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 
+// components
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton
+              text={"왼쪽 버튼"}
+              onClick={() => {
+                alert("왼쪽 버튼 클릭");
+              }}
+              type={"positive"}
+            />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => {
+                alert("오른쪽 버튼 클릭");
+              }}
+              type={"negative"}
+            />
+          }
+        />
         <h2>App.js</h2>
         <Routes>
           <Route path="/" element={<Home />} />
