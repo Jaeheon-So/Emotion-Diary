3846724d06204765431d2f1e39ef3d17e932e025
Jaeheon-So <sjh-98@naver.com>
2022-02-28 18:20:25 +0900

    Add New page date section

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,22 +1,9 @@
-import { useNavigate } from "react-router-dom";
-import MyButton from "../components/MyButton";
-import MyHeader from "../components/MyHeader";
+import DiaryEditor from "../components/DiaryEditor";
 
 const New = () => {
-  const navigate = useNavigate();
   return (
     <div>
-      <MyHeader
-        headText={"새 일기쓰기"}
-        leftChild={
-          <MyButton
-            text={"< 뒤로가기"}
-            onClick={() => {
-              navigate(-1);
-            }}
-          />
-        }
-      />
+      <DiaryEditor />
     </div>
   );
 };
