922b0f60e789009fb2a05bc448c7089be1f03ceb
Jaeheon-So <sjh-98@naver.com>
2022-02-28 18:58:38 +0900

    Add New page emotion section

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -2,6 +2,35 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MyButton from "./MyButton";
 import MyHeader from ".//MyHeader";
+import EmotionItem from "./EmotionItem";
+
+const emotionList = [
+  {
+    emotionId: 1,
+    emotionImg: process.env.PUBLIC_URL + `assets/emotion1.png`,
+    emotionDescript: "완전 좋음",
+  },
+  {
+    emotionId: 2,
+    emotionImg: process.env.PUBLIC_URL + `assets/emotion2.png`,
+    emotionDescript: "좋음",
+  },
+  {
+    emotionId: 3,
+    emotionImg: process.env.PUBLIC_URL + `assets/emotion3.png`,
+    emotionDescript: "그럭저럭",
+  },
+  {
+    emotionId: 4,
+    emotionImg: process.env.PUBLIC_URL + `assets/emotion4.png`,
+    emotionDescript: "나쁨",
+  },
+  {
+    emotionId: 5,
+    emotionImg: process.env.PUBLIC_URL + `assets/emotion5.png`,
+    emotionDescript: "끔찍함",
+  },
+];
 
 const getStringDate = (date) => {
   return date.toISOString().slice(0, 10);
@@ -10,6 +39,11 @@ const getStringDate = (date) => {
 const DiaryEditor = () => {
   const navigate = useNavigate();
   const [date, setDate] = useState(getStringDate(new Date()));
+  const [emotion, setEmotion] = useState(3);
+
+  const handleClickEmote = (emotion) => {
+    setEmotion(emotion);
+  };
 
   return (
     <div className="DiaryEditor">
@@ -34,6 +68,19 @@ const DiaryEditor = () => {
               onChange={(e) => setDate(e.target.value)}
               type="date"
             />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) => (
+              <EmotionItem
+                key={it.emotionId}
+                {...it}
+                onClick={handleClickEmote}
+                isSelected={it.emotionId === emotion}
+              />
+            ))}
           </div>
         </section>
       </div>
